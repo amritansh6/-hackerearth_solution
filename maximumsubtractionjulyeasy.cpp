505a34e7995cadb09fe9189a52010a62070e2dbf
Amritansh Mishra <amritansh40@gmail.com>
2019-07-08 21:25:59 +0530

    Create maximumsubtractionjulyeasy.cpp

---
+++ maximumsubtractionjulyeasy.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+ 
+#define FAST std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define DECIMAL(n)  std::cout << std::fixed;std::cout << std::setprecision(n);
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define fi first 
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))  
+#define REVERSE(v) reverse(ALL(v))
+#define endl "\n"
+#define maxc(v) max_element(all(v))
+#define minc(v) min_element(all(v))
+#define GCD(m,n) __gcd(m,n)
+#define debug cerr<<"bhau"<<endl
+#define debugv(x) cerr<<x<<endl
+#define inputarr(a,n) for(int i=0;i<n;++i) cin>>a[i]
+#define printarr(a,n) for(int i=0;i<n;++i) cout<<a[i]<<" "
+#define initarr(a,n,x) for (int i=0;i<n;++i) a[i]=x
+ 
+#define ll unsigned long long
+#define ld long double 
+#define pii pair<int, int> 
+#define pll pair<ll, ll>  
+#define vi vector<int>  
+#define vvi vector<vi > 
+#define vii vector<pii >
+using namespace std;
+ 
+int main()
+{
+long int n,m;
+long int k;
+cin>>n>>m;
+cin>>k;
+long int i=m;
+long int p=k;
+auto count=0;
+while(n>=0)
+{
+n=n-i;
+count++;
+if(n<0)
+{
+break;
+}
+if(p==0)
+{
+p=k;
+i=m;
+}
+else
+{
+p--;
+i--;
+}
+}
+cout<<count;
+return 0;
+}
