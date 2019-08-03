dcb757269ff950e9dbf44c67b19cad219b3c3e33
Amritansh Mishra <amritansh40@gmail.com>
2019-08-03 19:50:18 +0530

    Create codechefmsnsadm1.cpp

---
+++ codechefmsnsadm1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+ 
+#define FAST std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define DECIMAL(n)  std::cout << std::fixed;std::cout << std::setprecision(n);
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define fi first 
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))  
+#define REVERSE(v) reverse(ALL(v))
+#define endl "\n"
+#define maxc(v) max_element(all(v))
+#define minc(v) min_element(all(v))
+#define GCD(m,n) __gcd(m,n)
+#define debug cerr<<"bhau"<<endl
+#define debugv(x) cerr<<x<<endl
+#define inputarr(a,n) for(int i=0;i<n;++i) cin>>a[i]
+#define printarr(a,n) for(int i=0;i<n;++i) cout<<a[i]<<" "
+#define initarr(a,n,x) for (int i=0;i<n;++i) a[i]=x
+ 
+#define ll  long long
+#define ld long double 
+#define pii pair<int, int> 
+#define pll pair<ll, ll>  
+#define vi vector<int>  
+#define vvi vector<vi > 
+#define vii vector<pii >
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll a[n],b[n];
+        inputarr(a,n);
+        inputarr(b,n);
+        ll ans=-1000000000;
+        for(ll i=0;i<n;i++)
+        {
+            ll x=(a[i]*20)-(b[i]*10);
+            if(x<0)
+            x=0;
+             ans=max(ans,x);
+        }
+        cout<<ans<<endl;
+    }
+}
